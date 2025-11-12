c99af32c5b3d9491714ff608a8c352c3a458a237
Carlos <U202015274@upc.edu.pe>
2025-11-12 01:52:18 -0500

    feat: create registerdata interface

---
+++ src/domain/entities/RegisterData.ts
@@ -0,0 +1,13 @@
+export interface RegisterData {
+    name: string;
+    lastName: string;
+    email: string;
+    password: string;
+    phone: string;
+    gender: string
+    age: number;
+    birthday: string;
+    emergencyContact: string;
+    height: number;
+    weight: number;
+}
